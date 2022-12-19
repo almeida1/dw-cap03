147473a256590a1dde7f1b57c3dae5a101135820
Edson Saraiva de Almleida <engsoftware1@gmail.com>
2022-12-19 20:59:47 -0300

    Initial commit

---
+++ src/componentes/TabelaHead/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+const TabelaHead = () => (
+  <thead>
+    <tr>
+      <th colSpan="4"> Lista de Livros</th>
+    </tr>
+    <tr>
+      <th> ISBN </th>
+      <th> Titulo </th>
+      <th> Autor </th>
+      <th></th>
+    </tr>
+  </thead>
+);
